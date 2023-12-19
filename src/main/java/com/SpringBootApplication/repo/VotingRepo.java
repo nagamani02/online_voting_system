bef7fc57e36e2935b80701e429fc24b8af8cdbb3
sl062882 <sivaramakrishna.linga@cerner.com>
2023-12-19 16:40:13 -0600

    Base Branch

--- src/main/java/com/SpringBootApplication/repo/VotingRepo.java
+++
@@ -1,9 +0,0 @@
-package com.SpringBootApplication.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.SpringBootApplication.resource.VoterDto;
-import com.SpringBootApplication.resource.VoterEntity;
-
-public interface VotingRepo extends JpaRepository<VoterEntity, Integer> {
-
-}
--- src/main/java/com/SpringBootApplication/resource/DateValidator.java
+++
@@ -1,6 +0,0 @@
-package com.SpringBootApplication.resource;
-
-public interface DateValidator {
-	boolean isValid(String dateStr);
-
-}
